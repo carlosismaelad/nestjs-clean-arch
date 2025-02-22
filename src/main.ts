c645f71d6dbf4785b106551ee6f960acf6b68b3d
Carlos Dourado <carlosismaelad@outlook.com>
2025-02-22 13:39:38 -0300

    create project, install and configure Fastify

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import type { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
