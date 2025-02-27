de1974d17aeccc736923e5a21b810d4207d235eb
Carlos Dourado <carlosismaelad@outlook.com>
2025-02-26 21:33:42 -0300

    ClassValidatorFields - finishing unit tests

--- src/shared/domain/validators/__tests__/unit/class-validator-fields.spec.ts
+++ src/shared/domain/validators/__tests__/unit/class-validator-fields.spec.ts
@@ -6,8 +6,8 @@ class StubClassValidatorFields extends ClassValidatorFields<{
 }> {}
 
 describe('ClassValidatorFielda unit tests', () => {
-  const sut = new StubClassValidatorFields()
   it('Should initialize erros and validatedData with null', () => {
+    const sut = new StubClassValidatorFields()
     expect(sut.errors).toBeNull()
     expect(sut.validatedData).toBeNull()
   })
@@ -20,9 +20,21 @@ describe('ClassValidatorFielda unit tests', () => {
         constraints: { isRequired: 'test error' },
       },
     ])
+    const sut = new StubClassValidatorFields()
     expect(sut.validate(null)).toBeFalsy()
     expect(spyValidateSync).toHaveBeenCalled()
     expect(sut.validatedData).toBeNull()
     expect(sut.errors).toStrictEqual({ field: ['test error'] })
+  })
+
+  it('Should validate without errors', () => {
+    const spyValidateSync = jest.spyOn(libClassValidator, 'validateSync')
+    spyValidateSync.mockReturnValue([])
+    const sut = new StubClassValidatorFields()
+
+    expect(sut.validate({ fields: 'value' })).toBeTruthy()
+    expect(spyValidateSync).toHaveBeenCalled()
+    expect(sut.validatedData).toStrictEqual({ fields: 'value' })
+    expect(sut.errors).toBeNull()
   })
 })
