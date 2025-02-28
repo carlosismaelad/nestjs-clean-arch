e620509e2a85b05a4b0094df83836096deb2fe60
Carlos Dourado <carlosismaelad@outlook.com>
2025-02-28 19:26:48 -0300

    exporting ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { EnvConfigModule } from './shared/infra/env-config/env-config.module'
 import { UsersModule } from './users/infrastructure/users.module'
+import { ConfigService } from '@nestjs/config'
 
 @Module({
   imports: [EnvConfigModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
--- src/shared/infra/env-config/env-config.module.ts
+++ src/shared/infra/env-config/env-config.module.ts
@@ -1,20 +1,36 @@
-import { DynamicModule, Module } from '@nestjs/common'
+import { Module, DynamicModule } from '@nestjs/common'
+import {
+  ConfigModule,
+  ConfigService,
+  ConfigModuleOptions,
+} from '@nestjs/config'
 import { EnvConfigService } from './env-config.service'
-import { ConfigModule, ConfigModuleOptions } from '@nestjs/config'
 import { join } from 'node:path'
 
 @Module({
-  providers: [EnvConfigService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
+    }),
+  ],
+  providers: [ConfigService, EnvConfigService], // Adicionando ConfigService
+  exports: [ConfigService, EnvConfigService],
 })
-export class EnvConfigModule extends ConfigModule {
+export class EnvConfigModule {
   static forRoot<ValidationOptions extends Record<string, any>>(
     options?: ConfigModuleOptions<ValidationOptions>,
-  ): Promise<DynamicModule> {
-    return super.forRoot({
-      ...options,
-      envFilePath: [
-        join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
+  ): DynamicModule {
+    return {
+      module: EnvConfigModule,
+      imports: [
+        ConfigModule.forRoot({
+          ...options,
+          envFilePath: join(
+            __dirname,
+            `../../../../.env.${process.env.NODE_ENV}`,
+          ),
+        }),
       ],
-    })
+    }
   }
 }
